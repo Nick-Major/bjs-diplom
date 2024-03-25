5029780bc4b8086640d80349c92126b8594d64ac
Nick-Major <divanchik26@gmail.com>
2024-03-25 15:53:38 +0300

    Логин пейдж сделан полностью

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,8 +1,24 @@
 "use strict";
 
 const userForm = new UserForm();
-userForm.loginFormCallback = data => getData(data);
-const olegAuthorization = new ApiConnector.login({login: "oleg@demo.ru", password: "demo"}, response => console.log(response));
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response) => {
+        console.log(response);
+        if (response.success === true) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+    })
+}
 
-userForm.registerFormCallback = data => getData(data);
-const nikitaRegistration = new ApiConnector.register({login: "nikita", password: "abcd"}, response => console.log(response));
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, (response) => {
+        console.log(response);
+        if (response.success === true) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    })
+}
