65a5d76a51c755d4298335dbdd40db36d653a97e
Nick-Major <divanchik26@gmail.com>
2024-03-27 13:00:26 +0300

    Логин и хоум пейдж исправлены

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,8 +3,7 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
     ApiConnector.login(data, (response) => {
-        console.log(response);
-        if (response.success === true) {
+        if (response.success) {
             location.reload();
         } else {
             userForm.setLoginErrorMessage(response.error);
@@ -14,8 +13,7 @@ userForm.loginFormCallback = (data) => {
 
 userForm.registerFormCallback = (data) => {
     ApiConnector.register(data, (response) => {
-        console.log(response);
-        if (response.success === true) {
+        if (response.success) {
             location.reload();
         } else {
             userForm.setRegisterErrorMessage(response.error);
